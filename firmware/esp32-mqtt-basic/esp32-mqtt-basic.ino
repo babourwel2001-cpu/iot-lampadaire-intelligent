86b90947ff7392cf02cd7b3a459d70fcd957b8d8
braims_b <babourwel2001@gmail.com>
2026-09-16 01:43:55 +0200

    Update MQTT server IP address placeholder

--- firmware/esp32-mqtt-basic/esp32-mqtt-basic.ino
+++ firmware/esp32-mqtt-basic/esp32-mqtt-basic.ino
@@ -8,7 +8,7 @@ const char* ssid = "ssid";              // ← Partage mobile
 const char* password = "password";           // ← Mot de passe
 
 // ==================== CONFIGURATION MQTT ====================
-const char* mqtt_server = "10.202.1.227";   // ← IP du PC
+const char* mqtt_server = "10.202.1.xxx";   // ← IP du PC
 const int mqtt_port = 1883;
 const char* mqtt_topic_data = "lampadaire/1/data";
 const char* mqtt_topic_command = "lampadaire/1/command";
